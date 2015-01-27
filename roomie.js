153561ed201c78db37e4ec9f4e87e64f239a0c58
Stephan Gabler <stephan.gabler@gmail.com>
2015-01-26 18:41:17 -0800

    very quick and dirty *craigslist title extraction* test

--- roomie.js
+++ roomie.js
@@ -1,23 +1,63 @@
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function () {
+  this.render('home');
+});
+
+Offers = new Mongo.Collection("offers");
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
+  Template.home.events({
+    'submit .new-offer': function (event) {
       // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      console.log('clicked', event.target.url.value);
+      var url = event.target.url.value
+
+      var o = Offers.insert({
+        id: 1,
+        listingUrl: url,
+        createdAt: new Date()
+      });
+
+      var data = {
+        objectId: o,
+        url: url
+      }
+      Meteor.call("checkTwitter", data, function(error, results) {
+          console.log(results.content); //results.data should be a JSON object
+      });
+
+
+      return false;
     }
   });
+
+  Template.home.helpers({
+    offer: function () {
+      var a = Offers.findOne({id: 1})
+      console.log(a);
+      return a
+    }
+  })
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
+    Meteor.methods({
+        checkTwitter: function (data) {
+            console.log('got called with', data);
+            this.unblock();
+            Offers.update(data.objectId, {$set: {text: 'extracting'}});
+            var $ = cheerio.load(Meteor.http.get(data.url).content);
+            return Offers.update(data.objectId, {$set: {title: $('h2.postingtitle').text()}});;
+        }
+    });
+
   });
 }
