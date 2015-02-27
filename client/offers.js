1016ad9cc12c09fbde8b46c6291e582a55e590c9
Stephan Gabler <stephan.gabler@gmail.com>
2015-02-26 23:11:20 -0800

    add a very basic list navigation

--- client/offers.js
+++ client/offers.js
@@ -15,3 +15,31 @@ Template.offerRequest.events({
     return false;
   }
 })
+
+
+Template.offer.events({
+  'click .request-link': function (event) {
+    Session.set('showRequest', this);
+    console.log('showRequest', this);
+  },
+  'click .next-button': function (event) {
+    var requests = this.requests,
+        requestIds = this.requests.map(function (o) {return o._id}),
+        currentRequestId = Session.get('showRequest')._id,
+        currentIndex = _.indexOf(requestIds, currentRequestId);
+    Session.set('showRequest', requests[currentIndex + 1])
+  },
+  'click .prev-button': function (event) {
+    var requests = this.requests,
+        requestIds = this.requests.map(function (o) {return o._id}),
+        currentRequestId = Session.get('showRequest')._id,
+        currentIndex = _.indexOf(requestIds, currentRequestId);
+    Session.set('showRequest', requests[currentIndex - 1])
+  }
+})
+
+Template.offer.helpers({
+  showRequest: function () {
+    return Session.get('showRequest')
+  }
+})
--- routes.js
+++ routes.js
@@ -18,15 +18,15 @@ Router.route('/my/offers', function () {
   })
 })
 
-Router.route('/my/offers/:_id', function () {
+Router.route('/my/offers/:offerId', function () {
   this.render('offer', {
     data: function () {
-      var offer = Offers.findOne({_id: this.params._id});
-      var requests
+      var offer = Offers.findOne({_id: this.params.offerId});
+      var requests, nRequests;
 
       // TODO: find proper solution to wait before offer is ready
       if (offer) {
-        requests = Requests.find({offerId: offer._id})
+        requests = Requests.find({offerId: offer._id}).fetch()
       }
       return {
         offer: offer,
@@ -36,28 +36,6 @@ Router.route('/my/offers/:_id', function () {
   });
 });
 
-Router.route('/my/offers/:offerId/request/:requestId', function () {
-  this.render('offerRequest', {
-    data: function () {
-      var offer, request, messages;
-
-      offer = Offers.findOne({_id: this.params.offerId});
-      if (offer) {
-        request = Requests.findOne({_id: this.params.requestId})
-      }
-
-      if (request) {
-        messages = Messages.find({requestId: request._id})
-      }
-
-      return {
-        offer: offer,
-        request: request,
-        messages: messages
-      }
-    }
-  })
-})
 
 Router.route('/offers/:_slug', function () {
   this.render('publicOffer', {
